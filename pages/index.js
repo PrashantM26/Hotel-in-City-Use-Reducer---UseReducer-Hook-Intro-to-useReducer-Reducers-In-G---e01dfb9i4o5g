71bcc3226555651e6be0a53d5ad9c0bde94d1c25
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-09 19:42:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,33 +5,51 @@ const initialState = { hotels: [], filteredHotels: [] };
 function reducer(state, action) {
   switch (action) {
     case "FETCH_SUCCESS":
-     
+      return { ...state, hotels: action.payload };
     case "FILTER":
-     
+      const filteredHotels = state.hotels.filter(
+        (hotel) => hotel.city === action.payload
+      );
     default:
-   
+     return state;
   }
 }
+
+
+
+
+
 export default function Home() {
    const [state, dispatch] = useReducer(reducer, initialState);
- 
-
+   const [cityInput, setCityInput] = useState("");
 
 
   useEffect(() => {
-    dispatch({ type: "FILTER"});
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
+    })
+  }, []);
+
+
+  const handleCityChange = (e) => {
+    const city = e.target.value;
+    setCityInput(city);
+    dispatch({ type: "FILTER", payload: city });
+  };
 
   return (
     <div className="App">
       <input
         type="text"
         placeholder="Enter city name"
+        value={cityInput}
+        onChange={handleCityChange}
       />
-     
-        <p >hotel_name</p>
-     
+     {state.filteredHotels.map((hotel) => (
+        <p key={hotel.hotel_name}>{hotel.hotel_name}</p>
+     ))}
     </div>
   );
-  }
+}
   
