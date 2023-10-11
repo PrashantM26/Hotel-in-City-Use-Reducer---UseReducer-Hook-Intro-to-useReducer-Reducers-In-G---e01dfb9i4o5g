5f4a4bd30b5e10dee27b8193356d03ed0014137d
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-10-11 20:04:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect, useState } from "react";
+/*import React, { useReducer, useEffect, useState } from "react";
 
 const initialState = { hotels: [], filteredHotels: [] };
 
@@ -13,7 +13,74 @@ function reducer(state, action) {
     default:
      return state;
   }
+}*/
+
+
+
+
+
+
+
+
+
+
+import React, { useReducer, useEffect, useState } from "react";
+
+const initialState = { hotels: [], filteredHotels: [] };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_SUCCESS":
+      return { ...state, hotels: action.payload, filteredHotels: action.payload };
+    case "FILTER":
+      const filteredHotels = state.hotels.filter(
+        (hotel) => hotel.city === action.payload
+      );
+      return { ...state, filteredHotels };
+    default:
+      return state;
+  }
 }
+
+function HomePage() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [cityInput, setCityInput] = useState("");
+
+ 
+  useEffect(() => {
+    fetch("https://content.newtonschool.co/v1/pr/63b85bcf735f93791e09caf4/hotels")
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      })
+  }, []);
+
+ 
+  const handleCityInput = (event) => {
+    const cityName = event.target.value;
+    setCityInput(cityName);
+    dispatch({ type: "FILTER", payload: cityName });
+  };
+
+  return (
+    <div>
+      <h1>Hotel List</h1>
+      <input
+        type="text"
+        placeholder="Enter city name"
+        value={cityInput}
+        onChange={handleCityInput}
+      />
+      <ul>
+        {state.filteredHotels.map((hotel, index) => (
+          <li key={index}>{hotel.hotel_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default HomePage;
 
 
 
